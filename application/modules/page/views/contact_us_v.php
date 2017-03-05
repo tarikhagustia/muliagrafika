dc9595bf25bc60671f1c850cdab00bf94d0c3118
tarikhagustia <agustia.tarikh150@gmail.com>
2017-03-05 07:06:37 -0500

    Pr nomor 3 dan 5

--- application/modules/page/views/contact_us_v.php
+++ application/modules/page/views/contact_us_v.php
@@ -1,43 +1,43 @@
 <!-- Home Section -->
- 
+
 <!-- End Home Section -->
 <!-- Navigation panel -->
 <?php echo modules::run('mg_nav/get_nav'); ?>
 <section class="page-section bg-dark-alfa-70 parallax-3" data-background="images/full-width-images/section-bg-25.jpg" style="background-image: url(&quot;images/full-width-images/section-bg-25.jpg&quot;); background-position: 50% 23px;">
     <div class="relative container align-left">
-        
+
         <div class="row">
-            
+
             <div class="col-md-8">
                 <h1 class="hs-line-11 font-alt mb-20 mb-xs-0">Hubungi Kami</h1>
                 <div class="hs-line-4 font-alt">
-                    
+
                 </div>
             </div>
-            
+
             <div class="col-md-4 mt-30">
                 <div class="mod-breadcrumbs font-alt align-right">
-                    
+
                 </div>
-                
+
             </div>
         </div>
-        
+
     </div>
 </section>
 
 <section class="page-section" id="contact">
     <div class="container relative">
-        
+
         <h2 class="section-title font-alt mb-70 mb-sm-40">
             Punya pertanyaan ?
         </h2>
-        
+
         <div class="row mb-60 mb-xs-40">
-            
+
             <div class="col-md-8 col-md-offset-2">
                 <div class="row">
-                    
+
                     <!-- Phone -->
                     <div class="col-sm-6 col-lg-4 pt-20 pb-20 pb-xs-0">
                         <div class="contact-item">
@@ -53,7 +53,7 @@
                         </div>
                     </div>
                     <!-- End Phone -->
-                    
+
                     <!-- Address -->
                     <div class="col-sm-6 col-lg-4 pt-20 pb-20 pb-xs-0">
                         <div class="contact-item">
@@ -69,7 +69,7 @@
                         </div>
                     </div>
                     <!-- End Address -->
-                    
+
                     <!-- Email -->
                     <div class="col-sm-6 col-lg-4 pt-20 pb-20 pb-xs-0">
                         <div class="contact-item">
@@ -80,17 +80,32 @@
                                 Email
                             </div>
                             <div class="ci-text">
-                                <a href="mailto:muliagrafika_20@yahoo.com">muliagrafika_20@yahoo.com</a>
+                                <a href="mailto:muliagrafika_20@yahoo.com">muliagrafika_20@yahoo.com</a> / <a href="mailto:pemesanan@muliagrafika.com">pemesanan@muliagrafika.com</a>
                             </div>
                         </div>
                     </div>
                     <!-- End Email -->
-                    
+
                 </div>
             </div>
-            
+
         </div>
-        
-        
+
+
     </div>
+</section>
+
+<section class="page-section" id="google-maps">
+  <div class="relative container align-center">
+    <h2 class="section-title font-alt mb-70 mb-sm-40">
+        Temukan Kami
+    </h2>
+    <iframe
+      width="600"
+      height="450"
+      frameborder="0" style="border:0"
+      src="https://www.google.com/maps/embed/v1/place?key=AIzaSyChqfMiMewG5zwlRs-TMrvvw_FsRnAgOKw&q=Muliagrafika" allowfullscreen>
+    </iframe>
+  </div>
+
 </section>
